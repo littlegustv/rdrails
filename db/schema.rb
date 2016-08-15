f9bf9b753352f9662ed3c0b5c7fda7461ccc84ea
Benjamin Heller <echo29x@gmail.com>
2016-08-15 10:46:01 -0400

    updated to mysql, moved game to new project

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20160809152249) do
 
-  create_table "areas", force: :cascade do |t|
+  create_table "areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "character_skills", force: :cascade do |t|
+  create_table "character_skills", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "character_id"
     t.integer  "skill_id"
     t.integer  "percentage",   default: 75
@@ -26,23 +26,23 @@ ActiveRecord::Schema.define(version: 20160809152249) do
     t.datetime "updated_at",                null: false
   end
 
-  create_table "characters", force: :cascade do |t|
+  create_table "characters", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.text     "description",   limit: 65535
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
     t.integer  "created_by_id"
     t.integer  "stat_id"
-    t.boolean  "playable",      default: false
+    t.boolean  "playable",                    default: false
     t.string   "char_class"
-    t.integer  "level",         default: 1
-    t.integer  "experience",    default: 0
+    t.integer  "level",                       default: 1
+    t.integer  "experience",                  default: 0
     t.string   "short"
     t.string   "long"
-    t.text     "keywords"
+    t.text     "keywords",      limit: 65535
   end
 
-  create_table "equipment", force: :cascade do |t|
+  create_table "equipment", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "character_id"
     t.integer  "weapon_id"
     t.integer  "head_id"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20160809152249) do
     t.datetime "updated_at",   null: false
   end
 
-  create_table "exits", force: :cascade do |t|
+  create_table "exits", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "direction"
     t.integer  "destination_id"
     t.integer  "room_id"
@@ -58,12 +58,12 @@ ActiveRecord::Schema.define(version: 20160809152249) do
     t.datetime "updated_at",     null: false
   end
 
-  create_table "inventory_items", force: :cascade do |t|
+  create_table "inventory_items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "item_id"
     t.integer "character_id"
   end
 
-  create_table "items", force: :cascade do |t|
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "description"
     t.integer  "stat_id"
@@ -74,26 +74,26 @@ ActiveRecord::Schema.define(version: 20160809152249) do
     t.integer  "level",       default: 1
   end
 
-  create_table "mobiles", force: :cascade do |t|
+  create_table "mobiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "room_id",      null: false
     t.integer "character_id", null: false
     t.integer "user_id"
   end
 
-  create_table "room_items", force: :cascade do |t|
+  create_table "room_items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "room_id"
     t.integer "item_id"
   end
 
-  create_table "rooms", force: :cascade do |t|
+  create_table "rooms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.integer  "area_id"
   end
 
-  create_table "skills", force: :cascade do |t|
+  create_table "skills", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.integer  "cp",         default: 1
     t.integer  "level",      default: 1
@@ -101,7 +101,7 @@ ActiveRecord::Schema.define(version: 20160809152249) do
     t.datetime "updated_at",             null: false
   end
 
-  create_table "stats", force: :cascade do |t|
+  create_table "stats", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "hitpoints",       default: 100
     t.integer  "manapoints",      default: 80
     t.integer  "attackspeed",     default: 0
@@ -112,7 +112,7 @@ ActiveRecord::Schema.define(version: 20160809152249) do
     t.integer  "hitroll",         default: 1
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "",       null: false
     t.string   "encrypted_password",     default: "",       null: false
     t.string   "reset_password_token"
@@ -127,8 +127,8 @@ ActiveRecord::Schema.define(version: 20160809152249) do
     t.datetime "updated_at",                                null: false
     t.string   "role",                   default: "player"
     t.integer  "active_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
--- game/character.rb
+++
@@ -1,8 +0,0 @@
-class Character
-  attr_reader :name, :description, :stats
-  def initialize(name, description, stats)
-    @name = name
-    @description = description
-    @stats = stats
-  end
-end
